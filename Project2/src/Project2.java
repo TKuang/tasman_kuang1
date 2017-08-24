e12f0cff372ad48297452b02fd0e1f7016697d35
TKuang <19kuangt24@millshighsch.org>
2017-08-24 09:31:29 -0700

    third time's the charm

--- Project2/src/Project2.java
+++ Project2/src/Project2.java
@@ -1,9 +1,12 @@
-//comment
+/*Tasman Kuang
+ * August 24 2017
+ * compare print and println
+ */
 public class Project2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hi");
+		System.out.println("Tasman is amazing");
+		
 	}
 
 }
