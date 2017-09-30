60ac36a49d33a9ae83f70381bb908ee821d72da3
TKuang <31107156+TKuang@users.noreply.github.com>
2017-09-29 20:34:58 -0700

    Update Calculate.java

--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -5,62 +5,77 @@
 public class Calculate {
 	
 	public static int square (int num) {
+		//integer is multiplied by itself once, then square is returned
 		int square = num*num;
 		return square;
 	}
 	public static int cube (int num) {
+		//integer is multipled by itself twice, then cube is returned
 		int cube = num*num*num;
 		return cube;
 	}
 	public static double average (int num1, int num2) {
+		//two integers are added together, divided by the number of integers, then the average is returned
 		int average = (num1+num2)/2;
 		return average;
 	}
 	public static double average (int num1, int num2, int num3) {
+		//three integers are added together, divided by the number of integers, then the average is returned
 		int average = (num1+num2+num3)/3;
 		return average;
-				
 	}
 	public static double toDegrees (double num) {
+		//number of radians is multiplied by the ratio of 180 degrees over pi and converted into a degree measurement which is returned
 		double toDegrees= num*(180/3.14159);
 		return toDegrees;
 	}
 	public static double toRadians (double num) {
+		//degree measurement if multiplied by ratio of pi over 180 degrees and converted into number of radians, which is returned
 		double toRadians = num*(3.14159/180);
 		return toRadians;	
 	}
 	public static int discriminant (int a, int b, int c) {
+		//three coefficients from standard form equation are plugged into the discriminant b^2 * 4ac, result is returned
 		int discriminant = b*b-(4*a*c);
 		return discriminant;
 	}
 	public static String toImproperFrac (int whole, int num, int den) {
-		int  nume = whole * den + num;
-		String ImproperFrac = new String(nume + "/" + den);
+		// whole number is multiplied by denominator, then added to numerator of mixed fraction to find the numerator of improper fraction
+		int numerator = whole * den + num;
+		String ImproperFrac = numerator + "/" + den;
+		//initiated string prints out numerator of new improper fraction, then "/" and old denominator because denominator of mixed and improper fractions are same
 		return ImproperFrac;	
 	}
 	public static String toMixedNum (int num, int denom) {
+		// whole number of mixed fraction found by how many times numerator goes in denominator (AKA by dividing them)
 		int whole = num/denom;
 		int nume = num - whole * denom;
-		String toMixedNum = new String(whole + "_" + nume + "/" + denom);
+		//whole number is equivalent to the numerator value of itself times denominator, so leftover numerator in mixed fraction can be found by subtracting producting of whole # and denominator
+		String toMixedNum = whole + "_" + nume + "/" + denom;
+		//initiated string prints out whole number followed by numerator, /, and denominator, appearing as a mixed fraction
 		return toMixedNum;
 	}
 	public static String foil (int num1, int num2, int num3, int num4, int a) {
-		String equation = new String((num1*a + "^2" + num2 *(num3*a + num4)));
+		String equation = (num1*a + "^2" + num2 *(num3*a + num4));
+		// equation form of (num1*a + num2)(num3*a + num4) simplifies/foils to (num*a^2 + num2*num3*a + num2*num4) when fully distributed
 		return equation;
 		
 	}
 	public static Boolean isDivisibleBy (int num1, int num2) {
 		if (num1 % num2 == 0){
 			Boolean isDivisibleBy = true;
+			//num1 is divisible by num2 if the remainder of their division is 0
 		}
 		else {
 			Boolean isDivisibleBy = false;
+			//if there is a remainder, then they are not divisible
 		}
 		return isDivisibleBy;
 	}
 	public static double absValue (double num){
 		if (num <= 0){
 			return num * -1;
+			//multiplying a negative by negative returns a positive
 		}
 		else {
 			return num;
@@ -70,6 +85,7 @@ public class Calculate {
 	public static double max (double num1, double num2){
 		if (num1 >= num2){
 			return num1;
+			//return num1 if that double is greater than num2 as it would be the max
 		}
 		else {
 			return num2;
@@ -78,34 +94,57 @@ public class Calculate {
 	public static double max (double num1, double num2, double num3){
 	    if (num1 >= num2 && num1 >= num3){
 		    return num1;      
+		    //if num1 is greater than both num2 and num3, it would be the max and returned
 	       }
 	       else if(num2 >= num1 && num2 >= num3){
 		    return num2;
+		    //if num2 is greater than both num1 and num3, it would be the max and returned
 	       }
 	       else {
 	            return num3;
+		    //if num2 and num1 are not the max or equal to the others, than num3 would be the max and returned
 	       }
 	   }
 	public static int min (int num1, int num2){
 	       if (num1 <= num2){
 	           return num1;
+		   //if num1 is less than num2, it would be the minimum and returned
 	       }
 	       else{
 	           return num2;
 	       }
 	   }
 	public static double round2 (double num){
-	    if (num * 1000 % 10 < 5){
-	        return (num * 1000 - num * 1000 % 10)/1000;
-	       }
-	    else {
-	        return (10 - num * 1000 % 10 + num * 1000) / 1000;
-	       }
-	   }
+		//for negative number
+		if (num < 0){
+			num *= -1;
+			//converts number to positive if initially negative
+			if (num * 1000 % 10 < 5){
+				num = (num * 1000 - num * 1000 % 10)/1000;
+			}
+			else {
+				num = (10 - num * 1000 % 10 + num * 1000)/1000;
+			}
+			num *= -1;
+			//converts number back into negative as initially
+			return num;
+		}
+		//for positive number
+		else {
+			if (num * 1000 % 10 < 5){
+				return (num * 1000 - num * 1000 % 10)/1000;
+			}
+			else {
+				return (10 - num * 1000 % 10 + num * 1000)/1000;
+			}
+		}
+	}
 	public static double exponent (double num1, int num2){
 		double multiply = num1;
 		for (int i = num2; i > 0; i--){
+			//loops number of times of num2
 			multiply *= num1;
+			//num1 is multiplied by itself for num2 cycles
 		}
 		return multiply;
 	}
@@ -114,8 +153,9 @@ public class Calculate {
 		num1 = num;
 		int answer = num1 * num;
 		for (int i = num; i > 0; i--){
-			num--;
-			answer = num1 * num;
+			num1--;
+			answer = num * num1;
+			//the number enterred is multiplied by itself repeatedly, with the multiple increasing by incrememnts of 1 until it reaches 0
 		}
 		return answer;
 	}
@@ -124,15 +164,25 @@ public class Calculate {
 		if(num > 1){
 			for(int i = num - 1; i > 1; i --){
 				check = Calculate.isDivisibleBy(num, i);
+				//checks to see if any num is divisible by any number less than it besides 1
 				if (check == true){
 					return false;
+					//if num is divisible by a smaller number other than 1, then it is not prime
+				}
+				else {
+					return true;
 				}
 			}
 		}
+		else if (num = 1){
+			return true;
+			//number is prime if it's one
+		}
 		else {
 			return false;
+			//number is not prime if it's 0 or less
 		}
-		return true;
+		
 	}
 	public static int gcf (int num1, int num2){
 		while (num2 != 0){
@@ -169,8 +219,8 @@ public class Calculate {
 	public static String quadForm (int num1, int num2, int num3){
 		double quad = discriminant(num1, num2,num3);
 		if (quad < 0){
-			String not = "no real roots";
-			return not;
+			String result = "no real roots";
+			return result;
 		} 
 		else if (quad == 0){
 			double answer = round2(-num2/(2*num1));
@@ -180,11 +230,17 @@ public class Calculate {
 		else {
 			double answer1 = round2((-num2 + quad)/(2 * num1));
 			double answer2 = round2((-num2 - quad)/(2 * num1));
-			String result = (min(answer1, answer2)) + " and " + max(answer1, answer2);
+			if (max(answer1, answer2) = answer1){
+				String result = answer2 + " and " + answer1;
+			}
+			else {
+				String result = answer1 + " and " + answer2;
+			}
 			return result;
 		}	
 	}
 }
+
 			
 	
 				
