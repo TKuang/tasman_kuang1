9f69daf74ebf7692c589eb1f2f9ed6120c161e33
TKuang <31107156+TKuang@users.noreply.github.com>
2017-10-17 19:31:27 -0700

    Update Quadratic.java

--- Calculate Library/src/Quadratic.java
+++ Calculate Library/src/Quadratic.java
@@ -51,6 +51,36 @@ public class Quadratic
         }
         return round2(root);
     }
+	
+	public static String quadForm (int num1, int num2, int num3){
+	double quad = discriminant(num1, num2,num3);
+	if (quad < 0){
+		String result = "no real roots";
+		return result;
+	} 
+	else if (quad == 0){
+		double answer = round2(-num2/(2*num1));
+		String result = answer + "";
+		return result;
+	} 
+	else {
+		double answer1 = round2((-num2 + quad)/(2 * num1));
+		double answer2 = round2((-num2 - quad)/(2 * num1));
+		if (max(answer1, answer2) == answer1){
+			String result = answer2 + " and " + answer1;
+			return result;
+		}
+		else {
+			String result = answer1 + " and " + answer2;
+			return result;
+		}
+	}	
+	}
+
+	public static int discriminant (int a, int b, int c) {
+	int discriminant = b*b-(4*a*c);
+	return discriminant;
+	}
     
     public static String quadrDescriber(double a, double b, double c){
         String description = "Description of the graph of: \n" + "y = " + a + "x^2 + " + b + " + " + c + "\n\n";
@@ -69,24 +99,5 @@ public class Quadratic
         description += "Y-intercept: " + c + "\n";
         return description;
     }
-    public static String quadForm(double a, double b, double c){
-        if(discriminant(a, b, c) < 0){//if the output after calling the discriminant is a negative number
-            return "None";
-        }
-        else if(discriminant(a, b, c) == 0){
-            double root = round2(-b / (2 * a));
-            String answer = root + "";
-            return answer;
-        }
-        else{
-            double smallRoot = round2((-b + (sqrt(discriminant(a,b,c)))) / (2*a));//rounds answer to two decimal places
-            double bigRoot = round2((-b - (sqrt(discriminant(a,b,c)))) / (2*a));
-            String answer = smallRoot + " and " + bigRoot;
-            return answer;
-        }
-    }
 
-    public static double discriminant(double a, double b, double c){
-        return (b * b) - 4 * a * c;
-    }
 }
