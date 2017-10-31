9cfbbdade0abb86ee185d2dcea4b9721c0157107
TKuang <31107156+TKuang@users.noreply.github.com>
2017-10-30 22:51:19 -0700

    Update StringSplit.java

--- StringSplit.java
+++ StringSplit.java
@@ -38,15 +38,25 @@ public class Split {
 		public static void middleWithSpace(String sandwich) {
 			String[] split = sandwich.split(" ");
 			int count = 0;
-			for (int n: split) {
-				if (n == ) 
-					
+			int index2 = 0;
+			for (int i = 0; i < split.length; i++){
+				index2++;
+				if (split[i] == "bread"){
+					count++;
+				}
+				if (count == 2){
+					break;
 				}
 			}
-			
-
-			 
-			
-			
+			int index1 = 0;
+			for (int j = 0; j < split.length; j++){
+				index1++;
+				if (split[j] == "bread"){
+					break;
+				}
+			}
+			for (int k = index1+1; k < index2; k++){
+				System.out.println(split[k]);
+			}
 		}
 }
