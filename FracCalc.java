4ddf124497b548efed330d74474e245da1f7178d
TKuang <31107156+TKuang@users.noreply.github.com>
2017-11-14 09:23:15 -0800

    Update FracCalc.java

--- FracCalc.java
+++ FracCalc.java
@@ -1,1 +1,14 @@
+import.java.util;
 
+public clas FracCalc {
+  
+  public static void main (String[]args) {
+    Scanner userInput= newScanner(System.in);
+    System.out.println("Type an addition operation between two fractions");
+    String fractions = userInput.nextString();
+    System.out.print("Sum = " + produceAnswer(fractions));
+  }
+  
+  public static String produceAnswer(fractions){
+    
+    
