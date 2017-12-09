e4dc7341f50fdec4a80f6071ed0a2c0393efd880
TKuang <31107156+TKuang@users.noreply.github.com>
2017-12-08 23:43:44 -0800

    Update Calculate.java

--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -29,16 +29,16 @@ public class Calculate {
 		return average;
 	}
 	//returns angle measure in radians in degrees
-	public static double toDegrees (double num) {
+	public static double toDegrees (double radians) {
 		//angle measure of radians is multiplied by the ratio of 180 degrees over pi and converted into a degree measurement which is returned
-		double toDegrees= num*(180/3.14159);
-		return toDegrees;
+		double degrees= radians*(180/3.14159);
+		return degrees;
 	}
 	//returns angle measure in degrees in radians
-	public static double toRadians (double num) {
+	public static double toRadians (double degrees) {
 		//degree measurement is multiplied by ratio of pi over 180 degrees and converted into angle measurement in radians
-		double toRadians = num*(3.14159/180);
-		return toRadians;	
+		double radians = degrees*(3.14159/180);
+		return radians;	
 	}
 	//returns value of discriminant given  coefficients of quadratic equation
 	public static double discriminant (double a, double b, double c) {
@@ -47,20 +47,20 @@ public class Calculate {
 		return discriminant;
 	}
 	//returns improper fractions given mixed number
-	public static String toImproperFrac (int whole, int nume, int denom) {
+	public static String toImproperFrac (int whole, int numerator, int denominator) {
 		// whole number is multiplied by denominator, then added to numerator of mixed fraction to find the numerator of improper fraction
-		int numerator = whole * denom + nume;
-		String ImproperFrac = numerator + "/" + denom;
+		int finalNumerator = whole * denominator + numerator;
+		String improperFrac = finalNumerator + "/" + denominator;
 		//initiated string prints out numerator of new improper fraction, then "/" and old denominator because denominator of mixed and improper fractions are same
-		return ImproperFrac;	
+		return improperFrac;	
 	}
 	//returns mixed number given improper fraction
-	public static String toMixedNum (int num, int denom) {
+	public static String toMixedNum (int numerator, int denominator) {
 		// whole number of mixed fraction found by how many times numerator goes in denominator (AKA by dividing them)
-		int whole = num/denom;
-		int nume = num - whole * denom;
+		int whole = numerator/denominator;
+		int combinedNumerator = numerator - whole * denominator;
 		//whole number is equivalent to the numerator value of itself times denominator, so leftover numerator in mixed fraction can be found by subtracting producting of whole # and denominator
-		String toMixedNum = whole + "_" + nume + "/" + denom;
+		String toMixedNum = whole + "_" + combinedNumerator + "/" + denominator;
 		//initiated string prints out whole number followed by numerator, /, and denominator, appearing as a mixed fraction
 		return toMixedNum;
 	}
@@ -72,19 +72,19 @@ public class Calculate {
 	}
 	//determines divisibility of an integer by another
 	public static Boolean isDivisibleBy (int num1, int num2) {
-		Boolean IsDivisibleBy;
+		Boolean divisible;
 		if (num1 % num2 == 0){
-			IsDivisibleBy = true;
+			divisible = true;
 			//num1 is divisible by num2 if the remainder of their division is 0
 		}
 		else {
-			IsDivisibleBy = false;
+			divisible= false;
 			//if there is a remainder, then they are not divisible
 		}
 		if (num2 == 0){
 			throw new ArithmeticException("A result of a number divided by 0 is undefined");
 		}
-		return IsDivisibleBy;
+		return divisible;
 	}
 	//returns absolute value of specific number
 	public static double absValue (double num){
@@ -158,27 +158,27 @@ public class Calculate {
 		}
 	}
 	//returns result of a value raised to a positive integer power
-	public static double exponent (double num1, int num2){
-		double multiply = 1;
-		for (int i = 0; i < num2; i++){
+	public static double exponent (double base, int exponent){
+		double product = 1;
+		for (int i = 0; i < exponent; i++){
 			//loops number of times of num2
-			multiply *= num1;
+			product *= base;
 			//num1 is multiplied by itself for num2 cycles
 		}
-		
-		return multiply;
+		return product;
 	}
 	//returns factorial of specific value
 	 public static int factorial(int num){
-	        int product = 1;
+		 int product = 1;
 	        if(num == 0){
-	            return 0;
+	            product = 0;
+	            return product;
 	        }
 	        else if(num < 0){
 	            throw new IllegalArgumentException("Your input is a negative number");
 	        }
 	        else{
-	            for(int i = 1; i <= num; i++){
+	            for (int i = 1; i <= num; i++){
 	                product *= i;
 	            }
 	            return product;
@@ -186,13 +186,13 @@ public class Calculate {
 	    }
 	//determines whether or not a number is prime
 	public static boolean isPrime (int num){
-		boolean check;
+		boolean divisible;
 		boolean isPrime = true;
 		if(num > 1){
 			for(int i = num - 1; i > 1; i --){
-				check = Calculate.isDivisibleBy(num, i);
+				divisible = Calculate.isDivisibleBy(num, i);
 				//checks to see if any num is divisible by any number less than it besides 1
-				if (check == true){
+				if (divisible == true){
 					isPrime = false;
 					//if num is divisible by a smaller number other than 1, then it is not prime
 				}
@@ -214,10 +214,12 @@ public class Calculate {
 	//returns greatest common factor of two numbers
 	public static int gcf (int num1, int num2){
 		int temp = 0;
+		//creates temporary storage
 	    if (num2 > num1) {
 	         temp = num1;
 	         num1 = num2;
 	         num2 = temp;
+	    //swaps numbers, order of largest number doesn't matter
 	    }
 	    while (num2 !=0) {
 	        temp = num1 % num2;
@@ -226,18 +228,7 @@ public class Calculate {
 	    }
 	    int gcd = (int) absValue (num1);	
 	    return gcd;
-		/*while (num2 != 0){
-			if(isDivisibleBy(num1, num2)){
-				num1 = num2;
-				//returns integer if it is divisible by the other
-			}
-			else {
-				int num3 = num1;
-				num1 = num2;
-				num2 = num3 % num2;
-			}
-		}
-		return (int) absValue (num1);*/
+		
 	}
 	//returns square root of a specific number
 	public static double sqrt(double num){
@@ -274,19 +265,19 @@ public class Calculate {
 			//square root of negative integer is undefined, so no roots
 		} 
 		else if (quad == 0){
-			double answer = round2(-num2/(2*num1));
-			result = answer + "";
+			double root = round2(-num2/(2*num1));
+			result = root + "";
 			//discriminant of 0 will result in one root of -b/2a
 		} 
 		else {
-			double answer1 = round2((-num2 + quad)/(2 * num1));
-			double answer2 = round2((-num2 - quad)/(2 * num1));
+			double root1 = round2((-num2 + quad)/(2 * num1));
+			double root2 = round2((-num2 - quad)/(2 * num1));
 			//rounds roots to two decimal places
-			if (max(answer1, answer2) == answer1){
-				result = answer2 + " and " + answer1;
+			if (max(root1, root2) == root1){
+				result = root2 + " and " + root1;
 			}
 			else {
-				result = answer1 + " and " + answer2;
+				result = root1 + " and " + root2;
 			}
 		}
 		return result;
