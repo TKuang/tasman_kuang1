6379043deff44d7f50d8d5ee5a543c3c21ee9755
TKuang <31107156+TKuang@users.noreply.github.com>
2018-02-24 11:40:40 -0800

    Update Sphere.java

--- Sphere.java
+++ Sphere.java
@@ -1,1 +1,10 @@
-
+public class Sphere extends ThreeDShape{
+    private double radius;
+  
+    public Sphere(double radius){
+        this.radius = radius;
+        super(calcVolume(), calcSA());
+    }
+  
+    public double calcVolume(){
+        double volume = 4/3 * (math.pi
